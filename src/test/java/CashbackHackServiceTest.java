2f019307946ddd0dabf345268be231e7979906d5
Andrey <justupor@gmail.com>
2024-04-13 14:05:12 +0300

    Added tests on TestNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,40 @@
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void
+    testRemainLessThanBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 200;
+        int expectedRemain = 800;
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+
+    @Test
+    public void testRemainZeroAmount() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int expectedRemain = 1000;
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+
+    @Test
+    public void testRemainEqualToBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expectedRemain = 0;
+
+        int actualRemain = cashbackHackService.remain(amount);
+
+        Assert.assertEquals(actualRemain, expectedRemain);
+    }
+}
